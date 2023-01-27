ecff8da14b4c79b8b6c033ff6ca104d4efb6e22f
filippociandy10 <filippo.ciandy10@gmail.com>
2023-01-27 13:02:42 -0700

    Added licensing 2

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import socketserver
 import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
-# 
+# Copyright 2023 Filippo Ciandy
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
